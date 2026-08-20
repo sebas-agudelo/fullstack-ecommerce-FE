a4a7b617b30269e4c9737065f27da83880f6741b
Sebastian Agudelo <sebastianagudelo95@hotmail.com>
2026-08-20 16:02:40 +0200

    First commit

--- src/components/Common/Logo.jsx
+++ src/components/Common/Logo.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 
 export const Logo = () => {
     return (
-        <Link to={`/`} className="w-[110px] lg:w-[120px] block transition-all duration-150 ease-in md:hover:scale-105">
+        <Link to={`/`} className="w-[122px] lg:w-[132px] block transition-all duration-150 ease-in md:hover:scale-105">
             <img src="/Elegant_Minimalist_Calligraphy_Initials_logo-removebg-preview.png" alt="Logo" className="w-full" />
         </Link>
     )
